5ab45cf62ca08b4bf2925387a14a5620e767380b
ss-haze <ss-haze@users.no-reply.github.com>
2020-12-15 00:08:00 +0100

    minor change

--- ch02/2-34.cpp
+++ ch02/2-34.cpp
@@ -17,9 +17,9 @@ int main()
   a = 42; //ok, i = 42;
   b = 42; //ok, b is an int
   c = 42; //ok, c is an int
-  d = 42; //illegal. d is a ptr to int, and must be assigned an int address.
-  e = 42; //illegal. e is a pointer to a const int, and must be assigned an int address.
-  g = 42; //illegal. g is a const int reference to a const int, and cannot be assigned to.
+  //d = 42; illegal. d is a ptr to int, and must be assigned an int address.
+  //e = 42; illegal. e is a pointer to a const int, and must be assigned an int address.
+  //g = 42; illegal. g is a const int reference to a const int, and cannot be assigned to.
 
   return 0;
 }
