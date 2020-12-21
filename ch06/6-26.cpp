df5a2bacb93a9436fbd65860c6f179f69f4bfff8
ss-haze <ss-haze@users.no-reply.github.com>
2020-12-21 20:37:00 +0100

    ch06 adds

--- ch06/6-26.cpp
+++ ch06/6-26.cpp
@@ -3,18 +3,18 @@
 
 using std::string;
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char *argv[])
+{
+
   for (int i = 0; i < argc; ++i)
     std::cout << i << "\t" << argv[i] << std::endl;
 
-  string s= "hello";
+  string s = "hello";
   string *sp = &s;
-    std::cout <<*sp<<std::endl;
+  std::cout << *sp << std::endl;
 
-    const char c[] = "how";
-    std::cout <<*c<<std::endl;
+  const char c[] = "how";
+  std::cout << *c << std::endl;
 
-   
   return 0;
 }
